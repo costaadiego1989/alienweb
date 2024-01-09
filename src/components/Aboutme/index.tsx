c322f5b5b7b6fd91a4d31d48181badc08e48bae1
Diego Costa <costaadiego1989@gmail.com>
2024-01-08 22:29:15 -0300

    refactor: remove theme errors

--- src/components/Aboutme/index.tsx
+++ src/components/Aboutme/index.tsx
@@ -24,8 +24,8 @@ export const Aboutme = () => {
               Olá, sou um desenvolvedor Full Stack apaixonado por tecnologia,
               videogames, extraterrestres e tudo relacionado ao espaço. Tenho 1
               ano e meio de experiência como Supervisor de Marketing em uma
-              multinacional e mais 6 anos como contratado PJ. Já atuei como desenvolvedor
-              Full Stack, onde pude aprimorar minhas habilidades em diversas
+              multinacional e mais 6 anos como contratado PJ, Somando um total de quase 8 anos.
+              Já atuei como desenvolvedor Full Stack, onde pude aprimorar minhas habilidades em diversas
               tecnologias e ferramentas. Além disso, sou pai de um filho
               maravilhoso que é minha maior inspiração para continuar me
               desenvolvendo como profissional e pessoa. Estou sempre em busca de
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThemeProvider } from "styled-components";
-import { Theme } from "./styles/Theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ThemeProvider theme={Theme}>
     <App />
-  </ThemeProvider>
 );
